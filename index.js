42a92afa3077886a2b80fa8d9325248242a55edd
Diwakar Kumar <diwakarkumars1234@gmail.com>
2025-07-29 17:57:12 +0530

    bug is fixed

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const img1=document.getElementById('img1');
 const img2=document.getElementById('img2');
 const p1=document.getElementById('p1');
  const p2=document.getElementById('p2');
+ let turn="h";
 ///
 
 function reset(event){
     sc1=0;
+    turn="h";
     sc2=0;
    score1.innerHTML=`Score: ${sc1}`;
    score2.innerHTML=`Score: ${sc2}`;
@@ -26,19 +28,22 @@ function reset(event){
 
 }
 //
-
 const buttons=document.getElementById('buttons');
-buttons.addEventListener('click',(event)=>{
-     if(event.target.tagName=="BUTTON"){
+//
+function start(event){
+       if(event.target.tagName=="BUTTON"){
               win.innerHTML="";
                img1.style.transform="scale(1)";
                img2.style.transform="scale(1)";
                p1.innerHTML="";
                p2.innerHTML="";
           //
-          p1.innerHTML=`${event.target.id}`;
+           p1.innerHTML=`${event.target.id}`;
+           buttons.removeEventListener('click',start);
+          //
          setTimeout(()=>{
-          let idx=Math.floor(Math.random()*3);
+          let idx=Math.floor(Math.random()*17);
+          idx=idx%3;
           p2.innerHTML=`${arr[idx]}`;
           //winner decide;
           const win=document.getElementById('win');
@@ -60,9 +65,12 @@ buttons.addEventListener('click',(event)=>{
              if(p2.innerHTML=='rock'){sc2++; score2.innerHTML=`Score: ${sc2}`; win.innerHTML="You Lose";img1.style.transform="scale(0.8)";img2.style.transform="scale(1.3)";}
              else{sc1++; score1.innerHTML=`Score: ${sc1}`; win.innerHTML="You Win";img1.style.transform="scale(1.3)";img2.style.transform="scale(0.8)";}
           }
+          buttons.addEventListener('click',start);
          },2000)
      }
-})
+}
+
+buttons.addEventListener('click',start);
 
 //restart;
 restart.addEventListener('click',reset);
